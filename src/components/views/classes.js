de8863a88bc180db4d7e196fc2f6a7a81bc27244
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-02-12 00:20:47 -0500

    possible switch to es6 classes for teacher and student objects, made initial file for testing named classes.js

---
+++ src/components/views/classes.js
@@ -0,0 +1,115 @@
+import moment from "moment";
+
+class Person {
+  constructor({ _id, fname, lname, phone, img }) {
+    this._id = _id || "";
+    this.fname = fname;
+    this.lname = lname;
+    this.phone = phone;
+    this.img = img;
+  }
+  getFullName() {
+    return this.name + " " + this.lname;
+  }
+}
+
+class Teacher extends Person {
+  constructor({ _id, fname, lname, phone, lessons, hours, img }) {
+    super(_id, fname, lname, phone, img);
+    this.lessons = lessons || [];
+    this.hours = hours || [];
+    this.text = fname;
+    this.nStu = 0;
+    this.trConv = 0;
+    this.trFail = 0;
+  }
+  lessonsPerMonth() {
+    let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.lessons.forEach(item => {
+      const d = new Date(item.start);
+      arr[d.getUTCMonth()]++;
+    });
+    return arr;
+  }
+  getConvRate() {
+    return (this.trConv / (this.trConv + this.trFail)) * 100;
+  }
+  changeAvailability(hours) {
+    let newHours = Object.keys(hours).map(key => {
+      return {
+        daysOfWeek: [key],
+        startTime: hours[key].startTime,
+        endTime: hours[key].endTime
+      };
+    });
+    this.hours = newHours;
+    //makes this chainable!
+    return this;
+  }
+  checkAvailability(eventObject) {
+    const time = moment.utc(eventObject.start).format("HH:mm");
+    const day = moment.utc(eventObject.start).day();
+    let isAvailable = false;
+    this.hours.forEach(h => {
+      h.daysOfWeek.forEach(d => {
+        d = Number(d);
+        if (d === day) {
+          if (time >= h.startTime && time < h.endTime) {
+            isAvailable = true;
+          } else {
+            isAvailable = false;
+          }
+        }
+      });
+    });
+    return isAvailable;
+  }
+}
+class Student extends Person {
+  constructor({
+    fname,
+    lname,
+    phone,
+    img,
+    trial,
+    lessons,
+    instrument,
+    teacher
+  }) {
+    super(fname, lname, phone, img);
+    this.trial = trial;
+    this.lessons = lessons;
+    this.instrument = instrument;
+    this.teacher = teacher;
+  }
+}
+export const Event = {
+  title: "DEFAULT",
+  start: "",
+  end: "",
+  id: "",
+  backgroundColor: "",
+  borderColor: "",
+  create: function Event(eventObject) {
+    const { title, start, end, id, backgroundColor, borderColor } = eventObject;
+    let event = Object.create(this);
+    event.title = title || window.prompt("Name this event: ");
+    event.start = moment.utc(start).format() || this.start;
+    event.end =
+      end ||
+      moment
+        .utc(start)
+        .add(30, "minutes")
+        .format();
+    event.id = id || this.id;
+    event.backgroundColor = backgroundColor || this.backgroundColor;
+    event.borderColor = borderColor || this.borderColor;
+    return event;
+  },
+  changeColor: function() {
+    this.backgroundColor = this.backgroundColor !== "red" ? "red" : "";
+    this.borderColor = this.borderColor !== "red" ? "red" : "";
+    //make this chainable
+    return this;
+  }
+};
