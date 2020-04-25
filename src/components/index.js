700a0c9f4983deb9f0a03c457f745ad345b2a588
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-04-24 20:56:18 -0400

    update dep

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import PopModal from "./PopModal";
 import Button from "./Button";
 import MainMenu from "./MainMenu";
 import Modal from "./Modal";
-export { Header, Footer, PopModal, Button, MainMenu, Modal };
+export { Header, Footer, Button, MainMenu, Modal };
