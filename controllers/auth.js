085e47556f72724148fc46bd25b9666fa149b74a
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-08-28 01:29:04 -0400

    update lessons controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,22 @@
 module.exports = {
-  isLoggedIn: (req, res, next) => {
-    console.log('checking is logged in')
-    if (req.isAuthenticated()) {//hangs
-      console.log('check passed')
-      return next(); //non-negotiable
-    } else {
-      console.log('check failed')
-      res.status(400).send({message:'this is an error'});
-      return;
-    }
-  },
-  isAdmin: (req, res, next) => {
-    if (req.user.role !== "admin") {
-      res.redirect("/login");
-    } else {
-      return next();
-    }
-  },
+	isLoggedIn: (req, res, next) => {
+		console.log('checking is logged in');
+		if (req.isAuthenticated()) {
+			//hangs
+			console.log('check passed');
+			return next(); //non-negotiable
+		} else {
+			console.log('check failed');
+			res.status(400).send({ message: 'this is an error' });
+			return;
+		}
+	},
+
+	isAdmin: (req, res, next) => {
+		if (req.user.role !== 'admin') {
+			res.redirect('/login');
+		} else {
+			return next();
+		}
+	},
 };
--- controllers/lessons.js
+++ controllers/lessons.js
@@ -1,146 +1,71 @@
-const { studentModel, lessonModel } = require('../models');
+const { lessonModel } = require('../models');
 const fs = require('fs');
+
 const lessonsCtrl = {
-	getLessons: (req, res) => {
-		if (req.params.id) {
-			res.json([]);
-		} else {
-			lessonModel.find().exec((err, data) => {
+	getLessons: async (req, res) => {
+		try {
+			if (req.params.id) {
+				lessonModel.findById({ _id: req.params.id }).exec((err, data) => {
+					if (err) throw err;
+					res.json(data);
+				});
+			} else {
+				lessonModel.find().exec((err, data) => {
+					if (err) throw err;
+					res.json(data);
+				});
+			}
+		} catch (err) {
+			throw err;
+		}
+	},
+
+	postLessons: async (req, res) => {
+		try {
+			const { lesson } = req.body;
+			const newLesson = new lessonModel(lesson);
+			newLesson.save((err, success) => {
+				if (err) throw err;
+				res.json(success);
+			});
+		} catch (err) {
+			throw err;
+		}
+	},
+
+	updateLessons: (req, res) => {
+		try {
+			const { lesson } = req.body;
+			lessonModel
+				.findByIdAndUpdate({ _id: lesson._id }, { lesson })
+				.exec((err, success) => {
+					if (err) throw err;
+					if (!fs.existsSync('./logs')) {
+						fs.mkdirSync('./logs');
+					}
+					fs.appendFile(
+						'./logs/lesson.txt',
+						`${JSON.stringify(success)}\n`,
+						(err) => {
+							if (err) throw err;
+						}
+					);
+					res.json(success);
+				});
+		} catch (err) {
+			throw err;
+		}
+	},
+
+	deleteLessons: async (req, res) => {
+		try {
+			lessonModel.findByIdAndDelete({ _id: req.params.id }).exec((err, data) => {
 				if (err) throw err;
 				res.json(data);
 			});
+		} catch (err) {
+			throw err;
 		}
-		//placeholder
-	},
-	postLessons: (req, res) => {
-		const { event, stID } = req.body;
-		const {
-			start,
-			end,
-			id,
-			title,
-			backgroundColor,
-			borderColor,
-			instrument,
-			icon,
-			rate,
-		} = event;
-		studentModel
-			.updateOne(
-				//find where lessons's child element that matches id
-				{ _id: stID },
-				{
-					$push: {
-						lessons: {
-							title,
-							id: id,
-							start,
-							end,
-							backgroundColor,
-							borderColor,
-							instrument,
-							icon,
-							rate,
-						},
-					},
-				}
-			)
-			.exec((err, success) => {
-				if (err) throw err;
-				if (!fs.existsSync('./logs')) {
-					fs.mkdirSync('./logs');
-				}
-				fs.appendFile(
-					'./logs/lesson.txt',
-					` Added ID: ${JSON.stringify(id)}\t${JSON.stringify(
-						success
-					)}\n`,
-					(err) => {
-						if (err) throw err;
-					}
-				);
-				console.log(success);
-				res.json(success);
-			});
-	},
-	updateLessons: (req, res) => {
-		const {
-			start,
-			end,
-			id,
-			title,
-			backgroundColor,
-			borderColor,
-			instrument,
-			icon,
-			rate,
-		} = req.body.event;
-		studentModel
-			.updateOne(
-				//find where lessons's child element that matches id
-				{ lessons: { $elemMatch: { id: id } } },
-				//set the first child of lessons that matches id, to {stuff in here}
-				{
-					$set: {
-						'lessons.$': {
-							title,
-							id,
-							start,
-							end,
-							backgroundColor,
-							borderColor,
-							instrument,
-							icon,
-							rate,
-						},
-					},
-				}
-			)
-			.exec((err, success) => {
-				if (err) throw err;
-				if (!fs.existsSync('./logs')) {
-					fs.mkdirSync('./logs');
-				}
-				fs.appendFile(
-					'./logs/lesson.txt',
-					`Updated ID: ${JSON.stringify(id)}\t${JSON.stringify(
-						success
-					)}\n`,
-					(err) => {
-						if (err) throw err;
-					}
-				);
-				console.log(success);
-				res.json(success);
-			});
-	},
-	deleteLessons: (req, res) => {
-		console.log(req.params.id);
-		const id = req.params.id;
-		studentModel
-			//update the student, where the lesson is stored
-			.updateOne(
-				//find where lessons's child element that matches id
-				{ lessons: { $elemMatch: { id: id } } },
-				{ $pull: { lessons: { id: id } } }
-			)
-			.exec((err, success) => {
-				if (err) throw err;
-				if (!fs.existsSync('./logs')) {
-					fs.mkdirSync('./logs');
-				}
-				fs.appendFile(
-					'./logs/lesson.txt',
-					`Deleted Lesson ID: ${JSON.stringify(id)}\t${JSON.stringify(
-						success
-					)}\n`,
-					(err) => {
-						if (err) throw err;
-					}
-				);
-				console.log(success);
-				res.json(success);
-			});
 	},
 };
 
